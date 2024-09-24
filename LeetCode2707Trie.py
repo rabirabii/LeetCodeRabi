157cc5a6c2dcf2ffb7acf04683bb4879e56d74e8
rabirabii <wahyubudiman0624@gmail.com>
2024-09-24 16:12:46 +0700

    Solved Leetcode 2707 Problem

---
+++ LeetCode2707Trie.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode()
+        for word in words:
+            curr = self.root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.word = True
+
+
+# Memoization
+class Solution:
+    def minExtraChars(self, s: str, dictionary: list[str]) -> int:
+
+        dp = {len(s): 0}
+        trie = Trie(dictionary)
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+
+            res = 1 + dfs(i + 1)  # skip curr char
+            curr = trie
+            for j in range(i, len(s)):
+                if s[j] not in curr.children:
+                    break
+                curr = curr.children[s[j]]
+                if curr.word:
+                    res = min(res, dfs(j + 1))
+            dp[i] = res
+            return res
+
+        return dfs(0)
